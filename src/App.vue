e7291acbf5639061d84df5fd7e46264a9e115177
Eric <eric@kellyklee.com>
2020-07-29 15:28:27 -0700

    Add Quasar plugin, add basic card component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <template>
   <div id="app">
+      <!--
     <img alt="Vue logo" src="./assets/logo.png">
     <div v-for="item in data" :key="item.id">
         <h4>{{ item.title }}</h4>
         <small>{{ item.author }} - {{ item.publishedAt }}</small>
         <p>{{ item.description }}</p>
+    </div>
+    -->
+    <div class="q-pa-md row items-start q-gutter-md">
+        <q-card class="my-card" flat bordered>
+            <q-card-section horizontal>
+                <q-card-section class="q-pt-xs">
+                    <div class="text-overline">Overline</div>
+                    <div class="text-h5 q-mt-sm q-mb-xs">Title</div>
+                    <div class="text-caption text-grey">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    </div>
+                </q-card-section>
+
+                <q-card-section class="col-5 flex flex-center">
+                    <q-img
+                        class="rounded-borders"
+                        src="https://cdn.quasar.dev/img/parallax2.jpg"
+                    />
+                </q-card-section>
+            </q-card-section>
+        </q-card>
     </div>
   </div>
 </template>
