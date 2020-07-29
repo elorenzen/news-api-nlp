40b44ab4963a24a71dd9a2be122144e817de1f9f
Eric <eric@kellyklee.com>
2020-07-29 09:37:26 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div v-for="item in data" :key="item.id">
+        <h4>{{ item.title }}</h4>
+        <small>{{ item.author }} - {{ item.publishedAt }}</small>
+        <p>{{ item.content }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+const apiKey = '290befad42ad4ee99ddf698aeea1f543'
+const axios = require('axios')
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      data: null
+    }
+  },
+  mounted () {
+    const key = apiKey
+
+    axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${key}`).then(response => this.data = response.data.articles)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
