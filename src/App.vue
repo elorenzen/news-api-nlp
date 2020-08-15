64aeedad22dc2ebe4a9531481cd8a6b70c42d323
Eric <eric@kellyklee.com>
2020-08-14 19:43:53 -0700

    Change layout

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,12 @@
     <div id="app">
         <div class="col q-pa-md q-mx-xl" align="center">
             <div class="row">
-                <div class="col-4">
+                <div class="col-6">
                     <q-btn to="/authors" label="AuthorList" />
                 </div>
-                <div class="col-4">
-                    <q-btn to="/author/fdsjal" label="SelectedAuthor" />
-                </div>
-                <div class="col-4">
+                
+                <div class="col-6">
                     <q-btn to="/publications" label="PublicationList" />
-                </div>
-                <div class="col-4">
-                    <q-btn to="/publication/fdjaslk" label="SelectedPublication" />
                 </div>
             </div>
             <router-view />
