c0fc212cac36191ff492bc9d546c246c74cdf901
Eric <eric@kellyklee.com>
2020-08-20 20:52:14 -0700

    Render publication, selected publication articles

--- src/components/SelectedPublication.vue
+++ src/components/SelectedPublication.vue
@@ -1,6 +1,26 @@
 <template>
     <div>
         {{ selectedSource }}
+        <q-card class="my-card q-my-sm" flat bordered v-for="item in sourceArticles" :key="item.id" style="max-width: 60%">
+                <q-card-section horizontal>
+                    <q-card-section class="col-5 flex flex-center" v-if="typeof item.urlToImage === 'string'">
+                        <q-img
+                            class="rounded-borders"
+                            :src="item.urlToImage"
+                            height="200px"
+                            width="250px"
+                        />
+                    </q-card-section>
+
+                    <q-card-section class="q-pt-xs">
+                        <div class="text-overline">{{ item.author }}</div>
+                            <div class="text-h6 q-mt-sm q-mb-xs">
+                                <a :href="item.url" target="_blank" class="text-primary link" style="text-decoration: none">{{ item.title }}</a>
+                            </div>
+                        <div class="text-caption text-grey">{{ item.description }}</div>
+                    </q-card-section>
+                </q-card-section>
+            </q-card>
     </div>
 </template>
 
@@ -12,30 +32,35 @@ export default {
     data () {
         return {
             selectedSource: null,
-            sources: null
+            sources: null,
+            sourceArticles: null
+        }
+    },
+    methods: {
+        renderArticles () {
+            let domainParam = this.selectedSource.url.replace(/^(?:https?:\/\/)?(?:www\.)?/i, "").split('/')[0]
+            console.log(domainParam)
+            axios.get(`https://newsapi.org/v2/everything?domains=${domainParam}&apiKey=${apiKey}`).then(response => {
+                this.sourceArticles = response.data.articles
+            })
+            console.log('article response: ', this.sourceArticles)
         }
     },
     mounted () {
-        console.log(this.$route)
         let param = this.$route.params.id
 
         const key = apiKey
 
         axios.get(`https://newsapi.org/v2/sources?language=en&country=us&apiKey=${key}`).then(response => {
             this.sources = response.data.sources
-            console.log('response.data.sources: ', response.data.sources)
-            console.log('abc-news: ', this.sources[0])
-            console.log('param: ', param)
 
             this.sources.forEach(source => {
                 if (param === source.id) {
                     this.selectedSource = source
+                    this.renderArticles()
                 }
             })
-
-            console.log(this.selectedSource)
         })
-
     }
 }
 </script>
